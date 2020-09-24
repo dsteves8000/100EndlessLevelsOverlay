28b004cf85a4e7f33fef51e7539cbbe5881c6847
dsteves8000 <59595206+dsteves8000@users.noreply.github.com>
2020-09-23 23:55:03 -0700

    Update MM2.java

--- MM2.java
+++ MM2.java
@@ -3,34 +3,58 @@ import java.io.*;
 import java.text.*;
 import org.jnativehook.GlobalScreen; 
 import org.jnativehook.keyboard.NativeKeyEvent; 
-import org.jnativehook.keyboard.NativeKeyListener; 
+import org.jnativehook.keyboard.NativeKeyListener;
 import java.lang.Math;  
 public class MM2 implements NativeKeyListener 
 {    
-   public static void main(String [] args)    
-   {           
-      //tracker();    //Used for other purposes don't worry about this method   
-      //runningavg(); //See my comment in the runningavg method header
-      
+   public static void main(String [] args) throws FileNotFoundException, IOException
+   {          
+      //Reads the file with the WR and PB
+	   handlePBWR();
+	   loadPBWR(); 
       //Reads user input (currently set to work when the comma button is pressed, you might need help on setting this up if you don't know about classpaths
       try { GlobalScreen.registerNativeHook(); }        
       catch(Exception e) { e.printStackTrace(); }        
       GlobalScreen.getInstance().addNativeKeyListener(new MM2());     
    }
-   static File oldoutput;
-   static File output;
+   static File oldoutput, output, PBWR;
    static long seconds;
    static int totalclears = 0;
    static long starttime = System.currentTimeMillis(); 
    static long cooldown = System.currentTimeMillis(); 
    static ArrayList<Integer> times = new ArrayList<Integer>();
-   public void nativeKeyPressed(NativeKeyEvent e)   
+   static String thePBWR = "";
+   public static boolean handlePBWR() throws IOException
+   {
+	   PBWR = new File ("PBWR.txt");
+	   PBWR.createNewFile();
+	   return true;
+   }
+   public static void loadPBWR() throws FileNotFoundException
+   {
+	   Scanner in = new Scanner(PBWR);
+	   int c = 0;
+	   while(in.hasNextLine())
+	   {
+			 thePBWR += in.nextLine();
+			 if(c == 0)
+			 {
+				 thePBWR += " | ";
+				 c++;
+			 }
+	   }
+   }
+   public void nativeKeyPressed(NativeKeyEvent e)
    {          
       long time = (System.currentTimeMillis() - starttime)/1000;       
       long secs = time%60; String sds = ""; if(secs < 10) { sds = "0"; }       
       long minutes = (time/60)%60; String sdm = ""; if(minutes < 10) { sdm = "0"; }       
       long hours = time/3600;    
       String kP = NativeKeyEvent.getKeyText(e.getKeyCode());
+      if(kP.equals("Back Slash"))
+      {
+         System.exit(1);
+      }   
       long cd = System.currentTimeMillis() - cooldown;
       int seclast = ((int) cd ) / 1000;
       if(cd >= 20000)
@@ -55,22 +79,41 @@ public class MM2 implements NativeKeyListener
                }
             }
             totalclears++;
+               long pacetime = (time*100)/totalclears;      //long pacetime = (time*100)/totalclears;                
+               long pacesecs = pacetime%60;                 
+               String pacesds = "";                 
+               if(pacesecs < 10)                 
+               {                    
+                  pacesds = "0";                 
+               }                
+               long paceminutes = (pacetime/60)%60;                 
+               String pacesdm = "";                
+               if(paceminutes < 10)                 
+               {                   
+                  pacesdm = "0";                 
+               }                  
+               long pacehours = pacetime/3600;
+               long record = 5058;              
+               double tilPBavg =  (double)Math.round((record - time)/(double)(100 - totalclears) * 100d)/ 100d;
+               String plus = "";              
+               if(tilPBavg > 0)              
+               {                
+                  plus = "+";              
+               }
             String line = "Recent clear rates: ";
             line += "\n";
             line += Integer.toString(hourcount);
-            line += " clears / past hr";
+            line += " clears / past hr ";
             line += "\n";
             line += Integer.toString(tenmincount);
-            line += " clears / past 10 min";
+            line += " clears / past 10 min ";
             line += "\n";
             line += "Time since stream start: ";
-            line += Long.toString(hours);
+             line += "  ";
             line += ":";
-            line += sdm;
-            line += Long.toString(minutes);
+             line += "  ";
             line += ":";
-            line += sds;
-            line += Long.toString(secs);
+             line += "  ";
             line += " (+";
             line += Integer.toString(seclast);
             line += ")";
@@ -78,45 +121,29 @@ public class MM2 implements NativeKeyListener
             line += "Clears since stream start: ";
             line += totalclears;
             line += "\n";
+            line += "Current Pace: ";               
+            line += Long.toString(pacehours); //line += " ";               
+            line += ":";              
+            line += pacesdm;              
+            line += Long.toString(paceminutes); //line += " ";               
+            line += ":";               
+            line += pacesds;              
+            line += Long.toString(pacesecs); //line += " "; 
+            line += " ("; 
+            line += plus; line += tilPBavg; 
+            line += ")";             
+            line += "\n";
+            line += thePBWR;  line += "\n"; //line += "PB: 1:22:15 (Off-Stream) PB/WR: 1:24:18 (On-Stream)";
+            line += "\n";
             System.out.println(line);
-            WriteFile(line);
-            //System.out.println("Recent clear rates: " + "\n" + hourcount + " clears / past hr" + "\n" + tenmincount + " clears / past 10 min" + "\n" + "Time since stream start: " + hours + ":" + sdm + minutes + ":" + sds + secs + "\n");       
-         }    
+            WriteFile(line);    
+         }
       }
-         //System.out.println(counter + " " + hours + ":" + sdm + minutes + ":" + sds + seconds + " " + locations[sel] + ": " + count[sel] + "/" + max[sel]);
    }
    public void nativeKeyReleased(NativeKeyEvent e){}   
    public void nativeKeyTyped(NativeKeyEvent e){}    
-   /*public static void runningavg()   //Don't use this unless you can't figure out how to work with JNativeHook, this method is an outdated version to the new one above 
-   {       
-      long seconds;   
-      int startsec = (int) (System.currentTimeMillis()/1000);         
-      ArrayList<Integer> times = new ArrayList<Integer>();       
-      for(;;)       
-      {           		
-         Scanner sc = new Scanner(System.in);          
-         String str = sc.nextLine();           
-         int sec = (int) (System.currentTimeMillis()/1000);          
-         times.add(sec);          
-         Integer [] timearray = times.toArray(new Integer[times.size()]);          
-         int hourcount = 0;          
-         int tenmincount = 0;          
-         for(int i = 0; i < timearray.length; i++)          
-         {             
-            if((sec - timearray[i]) <= 3600)             
-            {                
-               hourcount++;                
-               if((sec - timearray[i]) <= 600)                
-               {                   
-                  tenmincount++;                
-               }             
-            }
-         }          
-         System.out.println("Recent clear rates: " + "\n" + hourcount + " clears / hr" + "\n" + tenmincount + " clears / 10 min" + "\n");       
-        }
-     }*/
-     public static void WriteFile(String line)
-     {
+   public static void WriteFile(String line)
+   {
          oldoutput = new File("Events.txt");
          if(oldoutput.canRead() == true)
          {
@@ -130,26 +157,5 @@ public class MM2 implements NativeKeyListener
             fW.close();
          }
          catch (IOException e) { e.printStackTrace(); }
-     }
-     public static void tracker()    
-     {       
-         long seconds;       
-         for(;;)       
-         {           		
-            Scanner sc = new Scanner(System.in);          
-            String str = sc.nextLine();           
-            seconds = System.currentTimeMillis();          
-            Date date = new Date(seconds);          
-            SimpleDateFormat sdf = new SimpleDateFormat("EEEE, MMMM d, yyyy h:mm:ss a", Locale.ENGLISH);          
-            sdf.setTimeZone(TimeZone.getTimeZone("PST"));         
-            String formattedDate = sdf.format(date);             
-            String[] nums = str.split("\\s+");          
-            int me   = Integer.parseInt(nums[0]);          
-            int them1 = Integer.parseInt(nums[1]);          
-            int them2 = Integer.parseInt(nums[2]);          
-            int diff1 = me - them1;          
-            int diff2 = me - them2;               
-            System.out.println(formattedDate + " " + me + " " + them1 + " " + them2 + "      " + diff1 + " " + diff2);       
-         }    
-    } 
+   }
 }
